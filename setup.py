7f3bfd285d8f105a4673599382533bfdbbdeec89
Alexey Kartashov <alexey.kartashov@scylladb.com>
2021-10-30 13:29:39 +0300

    feature(argus): bump version to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='argus',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
