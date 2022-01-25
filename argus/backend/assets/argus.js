498f541641b2c47ad21162fa5421171d690a2a57
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-01-25 19:41:49 +0300

    feature(IssueTemplate.svelte): Raw text and Separate Preview tab

--- argus/backend/assets/argus.js
+++ argus/backend/assets/argus.js
@@ -4,5 +4,5 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import 'marked';
+import 'github-markdown-css';
 import './argus.css';
-
