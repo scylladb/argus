d0044260fab3e60f40776b37bda5376696c88fb4
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-04-25 16:08:25 +0300

    feature(argus): Convert to poetry project

--- start_argus.sh
+++ start_argus.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export CQLENG_ALLOW_SCHEMA_MANAGEMENT=1
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-pyenv activate argus
-uwsgi --ini uwsgi.ini
+poetry run uwsgi --ini uwsgi.ini
