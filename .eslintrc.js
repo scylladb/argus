b4e7b648b1a4affe9fe0559eedfa8044217bf417
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-06-05 15:53:42 +0300

    improvement(argus): Add linter and formatter for JavaScript
    This commit adds ESLint for linting and enforcing a code style and
    Prettier code formatter for formatting files. It also adds matching
    configurations to them.

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "svelte3"
+    ],
+    "overrides": [
+        {
+            files: ["*.svelte"],
+            processor: "svelte3/svelte3"
+        }
+    ],
+    "settings": {
+        "svelte3/typescript": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
