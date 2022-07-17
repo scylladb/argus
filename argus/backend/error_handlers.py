d38c12331edaf957f706b24329ddddd3f7daf323
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-07-17 10:21:27 +0300

    refactor(argus.backend.controller.api): Use unified error handling

---
+++ argus/backend/error_handlers.py
@@ -0,0 +1,17 @@
+import logging
+from traceback import format_exception
+from flask import request
+
+LOGGER = logging.getLogger(__name__)
+
+
+def handle_api_exception(exception: Exception):
+    LOGGER.error("Exception in %s\n%s", request.endpoint, "".join(format_exception(exception)))
+
+    return {
+        "status": "error",
+        "response": {
+            "exception": exception.__class__.__name__,
+            "arguments": exception.args
+        }
+    }
