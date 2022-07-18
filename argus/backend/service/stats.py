48846aa32ae33e904cee7d18ec370b9de910e390
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-07-18 10:15:22 +0300

    refactor(models/web.py): Simplify Model Names

--- argus/backend/service/stats.py
+++ argus/backend/service/stats.py
@@ -3,8 +3,8 @@ import logging
 from datetime import datetime
 from argus.backend.models.sct_testrun import SCTTestRun
 from argus.backend.util.enums import TestStatus, TestInvestigationStatus
-from argus.backend.models.web import ArgusGithubIssue, ArgusRelease, ArgusReleaseGroup, ArgusReleaseGroupTest,\
-    ArgusReleaseScheduleTest, ArgusTestRunComment
+from argus.backend.models.web import ArgusGithubIssue, ArgusRelease, ArgusGroup, ArgusTest,\
+    ArgusScheduleTest, ArgusTestRunComment
 from argus.backend.db import ScyllaCluster
 
 LOGGER = logging.getLogger(__name__)
@@ -21,7 +21,7 @@ class ReleaseStats:
         self.has_bug_report = False
         self.issues: list[ArgusGithubIssue] = []
         self.comments: list[ArgusTestRunComment] = []
-        self.test_schedules: list[ArgusReleaseScheduleTest] = []
+        self.test_schedules: list[ArgusScheduleTest] = []
         self.forced_collection = False
 
     def to_dict(self) -> dict:
@@ -43,7 +43,7 @@ class ReleaseStats:
             return
 
         if not self.release.perpetual and not limited:
-            self.test_schedules = list(ArgusReleaseScheduleTest.filter(
+            self.test_schedules = list(ArgusScheduleTest.filter(
                 release_id=self.release.id
             ).all())
 
@@ -51,8 +51,8 @@ class ReleaseStats:
         if not limited or force:
             self.issues = ArgusGithubIssue.filter(release_id=self.release.id).all()
             self.comments = ArgusTestRunComment.filter(release_id=self.release.id).all()
-        self.all_tests = ArgusReleaseGroupTest.filter(release_id=self.release.id).all()
-        groups: list[ArgusReleaseGroup] = ArgusReleaseGroup.filter(release_id=self.release.id).all()
+        self.all_tests = ArgusTest.filter(release_id=self.release.id).all()
+        groups: list[ArgusGroup] = ArgusGroup.filter(release_id=self.release.id).all()
         for group in groups:
             if group.enabled:
                 stats = GroupStats(group=group, parent_release=self)
@@ -66,7 +66,7 @@ class ReleaseStats:
 
 
 class GroupStats:
-    def __init__(self, group: ArgusReleaseGroup, parent_release: ReleaseStats) -> None:
+    def __init__(self, group: ArgusGroup, parent_release: ReleaseStats) -> None:
         self.group = group
         self.parent_release = parent_release
         self.status_map = {status: 0 for status in TestStatus}
@@ -111,9 +111,9 @@ class GroupStats:
 class TestStats:
     def __init__(
         self,
-        test: ArgusReleaseGroupTest,
+        test: ArgusTest,
         parent_group: GroupStats,
-        schedules: list[ArgusReleaseScheduleTest] | None = None
+        schedules: list[ArgusScheduleTest] | None = None
     ) -> None:
         self.test = test
         self.parent_group = parent_group
--- argus/db/interface.py
+++ argus/db/interface.py
@@ -23,8 +23,8 @@ from argus.db.argus_json import ArgusJSONEncoder
 from argus.db.config import BaseConfig, FileConfig
 from argus.db.db_types import ColumnInfo, CollectionHint, ArgusUDTBase
 from argus.db.cloud_types import ResourceState
-from argus.backend.models.web import ArgusReleaseSchedule, ArgusReleaseScheduleAssignee, \
-    ArgusReleaseScheduleGroup, ArgusReleaseScheduleTest, ArgusRelease, ArgusReleaseGroup, ArgusReleaseGroupTest
+from argus.backend.models.web import ArgusSchedule, ArgusScheduleAssignee, \
+    ArgusSchedueGroup, ArgusScheduleTest, ArgusRelease, ArgusGroup, ArgusTest
 
 LOGGER = logging.getLogger(__name__)
 
@@ -50,12 +50,12 @@ class ArgusDatabase:
     CQL_ENGINE_CONNECTION_NAME = 'argus_cql_engine_conn'
     ARGUS_EXECUTION_PROFILE = "argus_named_tuple"
     REQUIRED_CQL_ENGINE_MODELS = [
-        ArgusReleaseSchedule,
-        ArgusReleaseScheduleGroup,
-        ArgusReleaseScheduleTest,
-        ArgusReleaseScheduleAssignee,
-        ArgusReleaseGroupTest,
-        ArgusReleaseGroup,
+        ArgusSchedule,
+        ArgusSchedueGroup,
+        ArgusScheduleTest,
+        ArgusScheduleAssignee,
+        ArgusTest,
+        ArgusGroup,
         ArgusRelease,
     ]
     PYTHON_SCYLLA_TYPE_MAPPING = {
