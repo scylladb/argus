42f38a923910f60b8a5611c293d84dad05495732
Alexey Kartashov <alexey.kartashov@scylladb.com>
2022-11-28 08:42:28 +0300

    feature(argus): Add profiling executable
    This executable module allows to run argus with profiler enabled -
    allowing to collect and view request profiles during development

---
+++ run_with_profiler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from werkzeug.middleware.profiler import ProfilerMiddleware
+from argus_backend import argus_app
+
+
+argus_app.wsgi_app = ProfilerMiddleware(app=argus_app.wsgi_app, profile_dir="profile",)
+
+argus_app.run(host="0.0.0.0", port=5000, debug=True,)
