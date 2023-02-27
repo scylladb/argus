32edbcc8c359e05ce8cd3c0f29397f731aa10ad0
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-02-27 15:29:10 +0400

    improvement(driver_matrix_tests.client): Adapters for xUnit artifacts
    This change introduces an adapter concept which takes the artifact xml
    produced by different driver matrices and returns adapted data with
    generic information that's needed for driver test submission

--- argus/client/driver_matrix_tests/client.py
+++ argus/client/driver_matrix_tests/client.py
@@ -1,4 +1,6 @@
 from functools import reduce
+from datetime import datetime
+from typing import TypedDict, Literal
 import re
 import logging
 from uuid import UUID
@@ -15,9 +17,56 @@ from argus.backend.plugins.driver_matrix_tests.raw_types import RawMatrixTestRes
 LOGGER = logging.getLogger(__name__)
 
 
+class AdaptedXUnitData(TypedDict):
+    timestamp: str
+
+
+def python_driver_matrix_adapter(xml: ElementTree.ElementTree) -> AdaptedXUnitData:
+    testsuites = list(xml.getroot().iter("testsuite"))
+
+    return {
+        "timestamp": testsuites[0].attrib.get("timestamp"),
+    }
+
+
+def java_driver_matrix_adapter(xml: ElementTree.ElementTree) -> AdaptedXUnitData:
+    testsuites = xml.getroot()
+    ts_now = datetime.utcnow().timestamp()
+    try:
+        time_taken = float(testsuites.attrib.get("time"))
+    except ValueError:
+        time_taken = 0.0
+
+    timestamp = datetime.utcfromtimestamp(ts_now - time_taken).isoformat()
+
+    return {
+        "timestamp": timestamp,
+    }
+
+
+def cpp_driver_matrix_adapter(xml: ElementTree.ElementTree) -> AdaptedXUnitData:
+    testsuites = xml.getroot()
+
+    return {
+        "timestamp": testsuites.attrib.get("timestamp"),
+    }
+
+
+def generic_adapter(xml: ElementTree.ElementTree) -> AdaptedXUnitData:
+    return {
+        "timestamp": datetime.utcnow().isoformat()
+    }
+
+
 class ArgusDriverMatrixClient(ArgusClient):
     test_type = "driver-matrix-tests"
     schema_version: None = "v1"
+
+    TEST_ADAPTERS = {
+        "java": java_driver_matrix_adapter,
+        "cpp": cpp_driver_matrix_adapter,
+        "python": python_driver_matrix_adapter,
+    }
 
     def __init__(self, run_id: UUID, auth_token: str, base_url: str, api_version="v1") -> None:
         super().__init__(auth_token, base_url, api_version)
@@ -68,21 +117,23 @@ class ArgusDriverMatrixClient(ArgusClient):
     def get_passed_count(self, suite_attribs: dict[str, str]) -> int:
         if (pass_count := suite_attribs.get("passed")):
             return int(pass_count)
-        total = int(suite_attribs.get("total", 0))
-        errors = int(suite_attribs.get("failures", 0))
+        total = int(suite_attribs.get("tests", 0))
+        errors = int(suite_attribs.get("errors", 0))
         skipped = int(suite_attribs.get("skipped", 0))
         failures = int(suite_attribs.get("failures", 0))
 
         return total - errors - skipped - failures
 
-    def parse_result_xml(self, xml_path: Path, env: dict[str, str] = None) -> RawMatrixTestResult:
+    def parse_result_xml(self, xml_path: Path, test_type:  Literal['java', 'cpp', 'python']) -> RawMatrixTestResult:
         with xml_path.open(mode="rt", encoding="utf-8") as xml_file:
             xml = ElementTree.parse(source=xml_file)
             LOGGER.info("%s", pformat(xml))
         testsuites = xml.getroot()
+        adapted_data = self.TEST_ADAPTERS.get(test_type, generic_adapter)(xml)
 
         driver_info = self.get_driver_info(xml_path.name)
         test_collection = {
+            "timestamp": adapted_data["timestamp"],
             "name": xml_path.stem,
             "driver_name": driver_info.get("driver_name"),
             "tests": 0,
@@ -94,9 +145,7 @@ class ArgusDriverMatrixClient(ArgusClient):
             "time": 0.0,
         }
         all_suites = []
-        for idx, suite in enumerate(testsuites.iter("testsuite")):
-            if idx == 0:
-                test_collection["timestamp"] = suite.attrib.get("timestamp")
+        for suite in testsuites.iter("testsuite"):
             raw_suite = {
                 "name": suite.attrib["name"],
                 "tests": int(suite.attrib.get("tests", 0)),
@@ -122,18 +171,18 @@ class ArgusDriverMatrixClient(ArgusClient):
             "suites": all_suites
         }
 
-    def get_results(self, result_path: str, env: dict[str, str] = None) -> list[RawMatrixTestResult]:
+    def get_results(self, result_path: str, test_type: Literal['java', 'cpp', 'python']) -> list[RawMatrixTestResult]:
         xmls = glob(f"{result_path}/**/*.xml", recursive=True)
         LOGGER.info("Will use following XMLs: %s", pformat(xmls))
         results = []
         for xml in xmls:
-            result = self.parse_result_xml(Path(xml), env=env)
+            result = self.parse_result_xml(Path(xml), test_type)
             results.append(result)
         return results
 
-    def submit(self, build_id: str, build_url: str, env_path: str, result_path: str):
+    def submit(self, build_id: str, build_url: str, env_path: str, result_path: str, test_type: Literal['java', 'cpp', 'python']):
         env = self.parse_build_environment(env_path)
-        results = self.get_results(result_path, env)
+        results = self.get_results(result_path, test_type)
 
         self.submit_driver_matrix_run(job_name=build_id, job_url=build_url, test_environment=env, results=results)
         failures = reduce(lambda total, coll: (coll["failures"] + coll["errors"]) + total, results, 0)
