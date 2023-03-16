a47667174c88f99f44600cbe4171dfe8487c8c28
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-03-16 07:58:01 +0300

    improvement(frontend): Add query-string package
    The package simplifies managing query string parameters compared to the
    standard URLSearchParams. In particular, it provides supports for all
    types of array specifications and quick parse back-and-forth. Currently
    only refactors use of WorkArea state management.

--- frontend/Common/StateManagement.js
+++ frontend/Common/StateManagement.js
@@ -1,14 +1,15 @@
 import { Base64 } from "js-base64";
+import queryString from "query-string";
 
 export const stateEncoder = function(rawState) {
     let state = JSON.stringify(rawState);
     let encodedState = Base64.encode(state, true);
-    return `state=${encodedState}`;
+    return encodedState;
 };
 
 export const stateDecoder = function () {
-    let params = new URLSearchParams(document.location.search);
-    let state = params.get("state");
+    let params = queryString.parse(document.location.search);
+    let state = params.state;
     if (state) {
         let decodedState = JSON.parse(Base64.decode(state));
         return decodedState;
