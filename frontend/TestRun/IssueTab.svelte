b6a878fb4acda450e92ed3c9a99f84c2150e3d46
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-05-24 12:43:06 +0200

    improvement(*TestRun): Allow viewing all issues relevant to the test
    This change adjust the Issues tab to display both issues attached to
    this particular run and a collapse of all issues associated with the
    run's test.
    
    Task: scylladb/qa-tasks#288

---
+++ frontend/TestRun/IssueTab.svelte
@@ -0,0 +1,22 @@
+<script>
+    import GithubIssues from "../Github/GithubIssues.svelte";
+
+    export let runId;
+    export let testInfo;
+</script>
+
+<GithubIssues id={runId} testId={testInfo.test.id} />
+<div class="accordion accordion-flush border-top" id="allIssuesContainer-{testInfo.test.id}-{runId}">
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#allIssues-{testInfo.test.id}-{runId}">
+            All issues for this test
+        </button>
+        </h2>
+        <div id="allIssues-{testInfo.test.id}-{runId}" class="accordion-collapse collapse" data-bs-parent="#allIssuesContainer-{testInfo.test.id}-{runId}">
+        <div class="accordion-body overflow-scroll" style="max-height: 768px">
+            <GithubIssues id={testInfo.test.id} testId={testInfo.test.id} filter_key="test_id" aggregateByIssue={true} submitDisabled={true}/>
+        </div>
+        </div>
+    </div>
+</div>
