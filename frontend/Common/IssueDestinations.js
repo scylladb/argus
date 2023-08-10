ed1f16c4a32c11674221f6c1e2e07f70107c119d
Artsiom Mishuta <artsiom.mishuta@scylladb.com>
2023-08-10 15:43:25 +0200

    fix(sirenada): open issues
    Add the ability to open issues to
    "siren", "siren-frontend" and "siren-devops"
    fixes https://github.com/scylladb/argus/issues/267

--- frontend/Common/IssueDestinations.js
+++ frontend/Common/IssueDestinations.js
@@ -1,18 +1,36 @@
-export const newIssueDestinations = [
-    {
-        "name": "ScyllaDB",
-        "url": "https://github.com/scylladb/scylla",
-    },
-    {
-        "name": "Scylla Enterprise",
-        "url": "https://github.com/scylladb/scylla-enterprise",
-    },
-    {
-        "name": "Scylla Cluster Tests",
-        "url": "https://github.com/scylladb/scylla-cluster-tests",
-    },
-    {
-        "name": "Sirenada",
-        "url": "https://github.com/scylladb/siren-tests",
-    }
-];
+import { PLUGIN_NAMES } from "./PluginNames";
+
+export const newIssueDestinations = {
+    [PLUGIN_NAMES.SCT]: [
+        {
+            name: "ScyllaDB",
+            url: "https://github.com/scylladb/scylla",
+        },
+        {
+            "name": "Scylla Enterprise",
+            "url": "https://github.com/scylladb/scylla-enterprise",
+        },
+        {
+            name: "Scylla Cluster Tests",
+            url: "https://github.com/scylladb/scylla-cluster-tests",
+        },
+    ],
+    [PLUGIN_NAMES.SIRENADA]: [
+        {
+            name: "Siren Tests",
+            url: "https://github.com/scylladb/siren-tests",
+        },
+        {
+            name: "Siren",
+            url: "https://github.com/scylladb/siren",
+        },
+        {
+            name: "Siren Frontend",
+            url: "https://github.com/scylladb/siren-frontend",
+        },
+        {
+            name: "Siren Devops",
+            url: "https://github.com/scylladb/siren-devops",
+        },
+    ],
+};
