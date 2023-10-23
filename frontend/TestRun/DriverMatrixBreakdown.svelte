9f646d4a01c2e60ab097b21e3e33f76b18354ef0
Alexey Kartashov <alexey.kartashov@scylladb.com>
2023-10-23 11:40:49 +0200

    refactor(frontend/DriverMatrix): Rework Driver Matrix Test Table
    This commit refactors the way driver matrix cases are presented to the
    user. The table have been simplified down to a simple row with relevant
    fields and a button to expand / hide additional information (Currently
    only contains a case message). Additionally, buttons for filtering
    statuses and a search bar for filtering specific tests have been added
    to each suite.
    
    Task: scylladb/qa-tasks#1513

---
+++ frontend/TestRun/DriverMatrixBreakdown.svelte
@@ -0,0 +1,196 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { TestCase, TestSuite } from "../Common/DriverMatrixTypes";
+    import { faAnglesDown, faAnglesUp, faCheck, faCopy, faTimes } from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
+    import { titleCase } from "../Common/TextUtils";
+    export let suites: TestSuite[] = [];
+    export let internalFilter = "";
+
+    interface ICaseSortPriorty {
+        [key: string]: number
+    }
+
+    interface IStatusDisplayInfo {
+        [key: string]: {
+            show: boolean,
+            testCount: number,
+            class: string,
+        }
+    }
+
+    interface ICaseExpansionInfo {
+        [key: string]: boolean,
+    }
+
+    const caseSortPriority: ICaseSortPriorty = {
+        failure: 0,
+        error: 1,
+        disabled: 7,
+        passed: 10,
+        skipped: 15,
+    };
+
+    const statusInfo: IStatusDisplayInfo = {
+        failure: {
+            show: true,
+            testCount: 0,
+            class: "bg-danger",
+        },
+        error: {
+            show: true,
+            testCount: 0,
+            class: "bg-warning",
+        },
+        passed: {
+            show: false,
+            testCount: 0,
+            class: "bg-success",
+        },
+        skipped: {
+            show: false,
+            testCount: 0,
+            class: "bg-secondary",
+        },
+        disabled: {
+            show: false,
+            testCount: 0,
+            class: "bg-dark",
+        },
+    };
+
+    const expandedCases: ICaseExpansionInfo = {};
+
+    const toggleStatus = function(status: string) {
+        statusInfo[status].show = !statusInfo[status].show;
+    };
+
+    const caseExpanded = function(testCase: TestCase, caseExpansionInfo: ICaseExpansionInfo) {
+        return !!(caseExpansionInfo[`${testCase.name}+${testCase.classname}`]);
+    };
+
+    const handleExpandButtonClick = function (caseName: string, className: string) {
+        const key = `${caseName}+${className}`;
+        expandedCases[key] = !expandedCases[key];
+    };
+
+    const handleCopyButtonClick = function(testCase: TestCase) {
+        navigator.clipboard.writeText(`Test: ${testCase.name}\nClass: ${testCase.classname}\n \`\`\`${testCase.message ?? "No message."}\`\`\`\n`);
+    };
+
+    const shouldFilter = function (testCase: TestCase, filterString: string) {
+        if (!filterString) return false;
+        try {
+            return !(new RegExp(filterString.toLowerCase()).test((testCase.name.toLowerCase() ?? "")));
+        } catch {
+            return false;
+        }
+    };
+
+    const displayStatus = function (testCase: TestCase, filteredStatuses: IStatusDisplayInfo) {
+        return filteredStatuses[testCase.status].show;
+    };
+
+    const aggregateSuites = function(suites: TestSuite[], statusInfo: IStatusDisplayInfo, filterString: string): TestCase[] {
+        return suites
+            .reduce<TestCase[]>((acc, suite) => [...acc, ...suite.cases], [])
+            .sort((a: TestCase, b: TestCase) => {
+                const lhs = caseSortPriority[a.status] ?? 99;
+                const rhs = caseSortPriority[b.status] ?? 99;
+                return lhs - rhs;
+            })
+            .filter((testCase) => displayStatus(testCase, statusInfo) && !shouldFilter(testCase, filterString));
+    };
+
+
+    const aggregateCaseCounts = function(suites: TestSuite[]) {
+        suites.forEach((suite) => {
+            suite.cases.forEach((testCase) => {
+                statusInfo[testCase.status].testCount++;
+                if (testCase.status == "failure" || testCase.status == "error") {
+                    handleExpandButtonClick(testCase.name, testCase.classname);
+                }
+            });
+        });
+    };
+
+    onMount(() => {
+        aggregateCaseCounts(suites);
+    });
+</script>
+
+<div class="d-flex my-2">
+    {#each Object.entries(statusInfo) as [status, displayInfo]}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="ms-1 rounded px-2 py-1 {displayInfo.class} text-white cursor-pointer" on:click={() => toggleStatus(status)}>
+            {#if displayInfo.show}
+                <Fa icon={faCheck}/>
+            {:else}
+                <Fa icon={faTimes} />
+            {/if}
+            {titleCase(status)}
+            ({displayInfo.testCount})
+        </div>
+    {/each}
+</div>
+<div class="mb-2">
+    <input class="form-control" placeholder="Filter tests" type="text" bind:value={internalFilter}>
+</div>
+<div>
+    {#each aggregateSuites(suites, statusInfo, internalFilter) as testCase}
+        <div class="mb-2">
+            <div class="mb-0 bg-light-three {caseExpanded(testCase, expandedCases) ? "rounded-top": "rounded"} p-2 border">
+                <div class="d-flex">
+                    <div>
+                        {testCase.name}
+                        <div class="text-small text-secondary">{testCase.classname}</div>
+                    </div>
+                    <div class="ms-auto rounded p-2 text-white {statusInfo[testCase.status].class}">
+                        {testCase.status.toUpperCase()}
+                    </div>
+                    {#if testCase.message}
+                    <button 
+                        class="ms-1 btn btn-primary" 
+                        title="More..." 
+                        on:click={() => handleExpandButtonClick(testCase.name, testCase.classname)}
+                    >
+                        {#if caseExpanded(testCase, expandedCases)}
+                            <Fa icon={faAnglesUp} />
+                        {:else}
+                            <Fa icon={faAnglesDown} />
+                        {/if}
+                    </button>
+                    {/if}
+                    <button class="ms-1 btn btn-success me-2" title="Copy all" on:click={() => handleCopyButtonClick(testCase)}>
+                        <Fa icon={faCopy} />
+                    </button>
+                </div>
+            </div>
+            {#if caseExpanded(testCase, expandedCases)}
+            <div class="border-start border-end border-bottom bg-white p-2">
+                {#if testCase.message}
+                    <pre class="bg-light-one rounded m-0 p-2 case-message">{testCase.message}</pre>
+                {:else}
+                    <div class="text-center m-0 text-secondary">No message</div>
+                {/if}
+            </div>
+            {/if}
+        </div>
+    {:else}
+        <div class="my-2 text-center text-secondary">No cases to show.</div>
+    {/each}
+</div>
+
+<style>
+    .text-small {
+        font-size: 0.8em;
+    }
+
+    .case-message {
+        white-space: pre-wrap
+    }
+
+    .cursor-pointer {
+        cursor: pointer;
+    }
+</style>
--- frontend/TestRun/DriverMatrixTestCollection.svelte
+++ frontend/TestRun/DriverMatrixTestCollection.svelte
@@ -1,52 +1,47 @@
 <script lang="ts">
     import Fa from "svelte-fa";
-    import { faCircle, faBoxes } from "@fortawesome/free-solid-svg-icons";
+    import { faCircle, faBoxes, faWarning, faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+    import DriverMatrixBreakdown from "./DriverMatrixBreakdown.svelte";
     import type { TestCollection } from "../Common/DriverMatrixTypes";
-    import DriverMatrixTestSuites from "./DriverMatrixTestSuites.svelte";
-    export let testCollection: TestCollection[];
+    export let collections: TestCollection[];
     export let testId: string;
 
-    const calculateCollectionStatus = function(collection: TestCollection): string {
-        return collection.failures > 0 || collection.errors > 0 ? "danger" : "success";
+    const calculateCollectionStatus = function(collection: TestCollection): boolean {
+        return collection.failures > 0 || collection.errors > 0;
+    };
+
+    const statusFilter = {
+        failure: true,
+        error: true,
     };
 </script>
 
-<div class="accordion" id="testCollectionAccordion">
-    {#each testCollection as testCol, idx}
+<div class="accordion accordion-flush">
+    {#each collections as collection, idx}
+    {@const isFailed = calculateCollectionStatus(collection)}
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingTestCollection-{testId}-{idx}">
                 <button
-                    class="accordion-button collapsed collection-{calculateCollectionStatus(testCol)}"
+                    class="accordion-button collapsed collection-{isFailed ? "danger" : "success"}"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#collapseTestCollection-{testId}-{idx}"
                 >
                     <div class="d-flex align-items-center flex-fill">
-                        <div class="ms-1">{testCol.name}</div>
-                        <div class="ms-auto p-1 p-help" title="Total">
-                            <Fa size="sm" icon={faBoxes}/> {testCol.tests_total}
-                        </div>
-                        <div class="ms-2 p-1 p-help" title="Passed">
-                            <Fa size="sm" class="text-success" icon={faCircle}/> {testCol.passed}
-                        </div>
-                        <div class="ms-2 p-1 p-help" title="Failed">
-                            <Fa size="sm" class="text-danger" icon={faCircle}/> {testCol.failures}
-                        </div>
-                        <div class="ms-2 p-1 p-help" title="Skipped">
-                            <Fa size="sm" class="text-secondary" icon={faCircle}/> {testCol.skipped}
-                        </div>
-                        <div class="ms-2 p-1 p-help" title="Errors">
-                            <Fa size="sm" class="text-warning" icon={faCircle}/> {testCol.errors}
-                        </div>
-                        <div class="ms-2 p-1 p-help" title="Disabled">
-                            <Fa size="sm" class="text-dark" icon={faCircle}/> {testCol.disabled}
-                        </div>
+                        <div class="ms-1">{collection.name}</div>
+                        {#if isFailed}
+                        {@const failureCount = collection.failures + collection.errors}
+                            <div class="ms-auto me-2 rounded">
+                                <Fa color="#fab73a" icon={faExclamationCircle}/>
+                                {failureCount} test{failureCount > 1 ? "s" : ""} failed
+                            </div>
+                        {/if}
                     </div>
                 </button>
             </h2>
-            <div id="collapseTestCollection-{testId}-{idx}" class="accordion-collapse collapse" data-bs-parent="#testCollectionAccordion">
+            <div id="collapseTestCollection-{testId}-{idx}" class="accordion-collapse collapse">
                 <div class="accordion-body" >
-                    <DriverMatrixTestSuites suites={testCol.suites} />
+                    <DriverMatrixBreakdown suites={collection.suites}/>
                 </div>
             </div>
         </div>
