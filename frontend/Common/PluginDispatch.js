e2ba7d1cd954b561b7476818fd9f45052b6e8633
Alexey Kartashov <alexey.kartashov@scylladb.com>
2024-03-04 13:34:31 +0100

    frontend(plugins/generic): Frontend components for Generic plugin
    This commit adds a simple frontent card for Generic plugin - currently
    nothing special, just usual information about the run - start time, end
    time, who started it, etc.

--- frontend/Common/PluginDispatch.js
+++ frontend/Common/PluginDispatch.js
@@ -2,11 +2,13 @@ import TestRun from "../TestRun/TestRun.svelte";
 import DriverMatrixTestRun from "../TestRun/DriverMatrixTestRun.svelte";
 import UnknownTest from "../WorkArea/UnknownTest.svelte";
 import SirenadaTestRun from "../TestRun/Sirenada/SirenadaTestRun.svelte";
+import GenericTestRun from "../TestRun/Generic/GenericTestRun.svelte";
 
 export const AVAILABLE_PLUGINS = {
     "scylla-cluster-tests": TestRun,
     "driver-matrix-tests": DriverMatrixTestRun,
     "sirenada": SirenadaTestRun,
+    "generic": GenericTestRun,
     unknown: UnknownTest,
 };
 
---
+++ frontend/TestRun/Generic/GenericTestRunInfo.svelte
@@ -0,0 +1,90 @@
+<script>
+    import {
+        faBusinessTime,
+    } from "@fortawesome/free-solid-svg-icons";
+    import humanizeDuration from "humanize-duration";
+    import Fa from "svelte-fa";
+    import { timestampToISODate } from "../../Common/DateUtils";
+    export let test_run = {};
+    export let release;
+    export let group;
+    export let test;
+</script>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6 p-2">
+            <h5>Run Details</h5>
+            <ul class="list-unstyled border-start ps-2">
+                <li>
+                    <span class="fw-bold">Release:</span>
+                    {release?.name ?? "#NO_RELEASE"}
+                </li>
+                <li>
+                    <span class="fw-bold">Group:</span>
+                    {(group?.pretty_name || group?.name) ?? "#NO_GROUP"}
+                </li>
+                <li>
+                    <span class="fw-bold">Test:</span>
+                    {(test?.pretty_name || test?.name) ?? "#NO_TEST"}
+                </li>
+                <li>
+                    <span class="fw-bold">Id:</span>
+                    {test_run.id}
+                </li>
+                <li>
+                    <span class="fw-bold">Start time:</span>
+                    {timestampToISODate(test_run.start_time, true)}
+                </li>
+                {#if new Date(test_run.end_time).getUTCFullYear() != 1970}
+                    <li>
+                        <span class="fw-bold">End time:</span>
+                        {timestampToISODate(test_run.end_time, true)}
+                    </li>
+                    <li>
+                        <span class="fw-bold">Duration:</span>
+                        {humanizeDuration(
+                            new Date(test_run.end_time) -
+                                new Date(test_run.start_time),
+                            {
+                                round: true,
+                                units: ["y", "mo", "w", "d", "h", "m"],
+                                largest: 1,
+                            }
+                        )}
+                    </li>
+                {/if}
+                <li>
+                    <span class="fw-bold">Started by:</span>
+                    {test_run.started_by ?? "Unknown, probably jenkins"}
+                </li>
+                <li>
+                    <span class="fw-bold">Build job:</span>
+                    <a href={test_run.build_job_url} target="_blank">
+                        {test_run.build_id}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-6 p-2">
+            <h5>Build Info</h5>
+            <ul class="list-unstyled border-start ps-2">
+                <li>
+                    <span class="fw-bold">Scylla Version:</span>
+                    {test_run.scylla_version}
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 p-2">
+            <div class="btn-group">
+                <a
+                    href="/dashboard/{release.name}"
+                    class="btn btn-outline-success"
+                    ><Fa icon={faBusinessTime} /> Release Dashboard</a
+                >
+            </div>
+        </div>
+    </div>
+</div>
