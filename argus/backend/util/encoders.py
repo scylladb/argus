7e258be4db2cd3d6b7dd165c084beb83a758c535
Alexey Kartashov <alexey.kartashov@scylladb.com>
2024-05-29 17:44:58 +0200

    feature(argus): Update argus dependencies (Flask 3.0.x)
    This commit relaxes some of the poetry dependencies and additionally
    updates Flask and other components to their most recent counterparts.
    Due to certain deprecations, a new ArgusJSONProvider class is now
    replacing previous serializer for Argus objects.

--- argus/backend/util/encoders.py
+++ argus/backend/util/encoders.py
@@ -3,6 +3,7 @@ import logging
 from json.encoder import JSONEncoder
 from uuid import UUID
 
+from flask.json.provider import DefaultJSONProvider
 import cassandra.cqlengine.usertype as ut
 import cassandra.cqlengine.models as m
 
@@ -10,6 +11,22 @@ import cassandra.cqlengine.models as m
 LOGGER = logging.getLogger(__name__)
 
 class ArgusJSONEncoder(JSONEncoder):
+    def default(self, o):
+        match o:
+            case UUID():
+                return str(o)
+            case ut.UserType():
+                return dict(o.items())
+            case m.Model():
+                return dict(o.items())
+            case datetime():
+                return o.strftime("%Y-%m-%dT%H:%M:%SZ")
+            case _:
+                return super().default(o)
+
+
+class ArgusJSONProvider(DefaultJSONProvider):
+
     def default(self, o):
         match o:
             case UUID():
