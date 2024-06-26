32cd16d9917d5036a046ebaf0ab21f86b3968a3e
Alexey Kartashov <alexey.kartashov@scylladb.com>
2024-06-26 08:01:58 +0200

    refactor(plugins/driver_matrix_tests): Multi-stage pipeline for Drivers
    This commit reworks the way Driver Matrix Runs are submitted to Argus,
    replacing previously one-shot architecture with a multi-stage pipeline
    akin to SCT, where driver matrix run first submits itself, and then
    submits results sequentially (either a pass or failure), finalizing with
    a call that makes backend determine status based on resulting payload.
    Patch failures are now supported. Entire logic was moved from the client
    to the backend, removing the need to update the client in case of large
    changes to the API / submitted data.
    
    Fixes #289

--- argus/backend/plugins/driver_matrix_tests/controller.py
+++ argus/backend/plugins/driver_matrix_tests/controller.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request
 
 from argus.backend.error_handlers import handle_api_exception
+from argus.backend.plugins.driver_matrix_tests.raw_types import DriverMatrixSubmitEnvRequest, DriverMatrixSubmitFailureRequest, DriverMatrixSubmitResultRequest
 from argus.backend.service.user import api_login_required
 from argus.backend.plugins.driver_matrix_tests.service import DriverMatrixService
+from argus.backend.util.common import get_payload
 
 bp = Blueprint("driver_matrix_api", __name__, url_prefix="/driver_matrix")
 bp.register_error_handler(Exception, handle_api_exception)
@@ -18,6 +20,43 @@ def driver_matrix_test_report():
     
 
     result = DriverMatrixService().tested_versions_report(build_id=build_id)
+    return {
+        "status": "ok",
+        "response": result
+    }
+
+@bp.route("/result/submit", methods=["POST"])
+@api_login_required
+def submit_result():
+    payload = get_payload(request)
+    request_data = DriverMatrixSubmitResultRequest(**payload)
+
+    result = DriverMatrixService().submit_driver_result(driver_name=request_data.driver_name, driver_type=request_data.driver_type, run_id=request_data.run_id, raw_xml=request_data.raw_xml)
+    return {
+        "status": "ok",
+        "response": result
+    }
+
+
+@bp.route("/result/fail", methods=["POST"])
+@api_login_required
+def submit_failure():
+    payload = get_payload(request)
+    request_data = DriverMatrixSubmitFailureRequest(**payload)
+
+    result = DriverMatrixService().submit_driver_failure(driver_name=request_data.driver_name, driver_type=request_data.driver_type, run_id=request_data.run_id, failure_reason=request_data.failure_reason)
+    return {
+        "status": "ok",
+        "response": result
+    }
+
+@bp.route("/env/submit", methods=["POST"])
+@api_login_required
+def submit_env():
+    payload = get_payload(request)
+    request_data = DriverMatrixSubmitEnvRequest(**payload)
+
+    result = DriverMatrixService().submit_env_info(run_id=request_data.run_id, raw_env=request_data.raw_env)
     return {
         "status": "ok",
         "response": result
