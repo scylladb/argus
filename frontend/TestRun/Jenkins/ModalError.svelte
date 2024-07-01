1612cf397bc3f578946a967d4ef05396c62edd9a
Alexey Kartashov <alexey.kartashov@scylladb.com>
2024-07-01 14:13:35 +0200

    improvement(TestRun/Jenkins): Add error handling inside modal window
    This commit changes the way errors are presented to the user during
    rebuild and clone process, removing old notification toasts and instead
    inlining those errors inside the modal windows themselves, allowing
    better readability and easier error reporting.

---
+++ frontend/TestRun/Jenkins/ModalError.svelte
@@ -0,0 +1,12 @@
+<script>
+    /**
+     * @type {{
+     *  message: string,
+     * }}
+     */
+    export let args;
+</script>
+
+<div class="alert alert-danger">
+    <pre style="white-space: pre-wrap;">{args.message}</pre>
+</div>
