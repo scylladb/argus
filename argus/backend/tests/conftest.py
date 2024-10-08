595e204cdc8e16d65c28119c00782e7d2a5fe92e
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-10-08 12:22:10 +0200

    fix(tests): add missing test files
    Some test files were missing from previous commit.
    Added.

---
+++ argus/backend/tests/conftest.py
@@ -0,0 +1,159 @@
+import os
+import time
+import uuid
+
+from cassandra.auth import PlainTextAuthProvider
+from docker import DockerClient
+
+os.environ['DOCKER_HOST'] = ""
+from _pytest.fixtures import fixture
+from docker.errors import NotFound
+from argus.backend.cli import sync_models
+from argus.backend.db import ScyllaCluster
+from argus.backend.models.web import ArgusTest, ArgusGroup, ArgusRelease
+from argus.backend.plugins.sct.testrun import SCTTestRunSubmissionRequest
+from argus.backend.service.client_service import ClientService
+from argus.backend.service.release_manager import ReleaseManagerService
+from argus.backend.service.results_service import ResultsService
+import logging
+from cassandra.cluster import Cluster
+logging.getLogger().setLevel(logging.INFO)
+os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
+logging.getLogger('cassandra').setLevel(logging.WARNING)
+logging.getLogger('cassandra.connection').setLevel(logging.WARNING)
+logging.getLogger('cassandra.pool').setLevel(logging.WARNING)
+logging.getLogger('cassandra.cluster').setLevel(logging.WARNING)
+
+
+def truncate_all_tables(session):
+    for table in session.cluster.metadata.keyspaces[session.keyspace].tables:
+        session.execute(f"TRUNCATE {table}")
+
+
+@fixture(scope='session')
+def argus_db():
+    container_name = "argus_test_scylla"
+    docker_client = DockerClient.from_env()
+    need_sync_models = True
+    try:
+        container = docker_client.containers.get(container_name)
+        if container.status != 'running':
+            container.start()
+            print(f"Started existing container '{container_name}'.")
+        else:
+            print(f"Using already running container '{container_name}'.")
+        need_sync_models = False
+    except NotFound:
+        container = docker_client.containers.run(
+            "scylladb/scylla-enterprise:2024.1.10",
+            name=container_name,
+            detach=True,
+            ports={'9042/tcp': 9042},
+            command=[
+                "--smp", "1",
+                "--overprovisioned", "1",
+                "--skip-wait-for-gossip-to-settle", "0",
+                "--endpoint-snitch=SimpleSnitch",
+                "--authenticator", "PasswordAuthenticator",
+            ],
+        )
+        log_wait_timeout = 120
+        start_time = time.time()
+
+        print("Waiting for 'init - serving' message in container logs...")
+        for log in container.logs(stream=True):
+            log_line = log.decode('utf-8')
+            if "init - serving" in log_line:
+                print("'init - serving' message found.")
+                break
+            if "FATAL state" in log_line:
+                raise Exception("ScyllaDB exited unexpectedly. Check container logs for more information.")
+            if time.time() - start_time > log_wait_timeout:
+                raise Exception("ScyllaDB did not log 'init - serving' within the timeout period.")
+
+    container.reload()
+    container_ip = container.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+    print(f"Container IP: {container_ip}")
+
+    if need_sync_models:
+        auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
+        cluster = Cluster([container_ip], port=9042, auth_provider=auth_provider)  # Use container IP
+        session = cluster.connect()
+        session.execute("""
+             CREATE KEYSPACE IF NOT EXISTS test_argus
+             WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};
+         """)
+    config = {"SCYLLA_KEYSPACE_NAME": "test_argus","SCYLLA_CONTACT_POINTS": [container_ip],
+              "SCYLLA_USERNAME": "cassandra", "SCYLLA_PASSWORD": "cassandra"}
+
+    database = ScyllaCluster.get(config)
+    if need_sync_models:
+        sync_models()
+    # truncate_all_tables(database.session)
+    session = database.cluster.connect(keyspace=config["SCYLLA_KEYSPACE_NAME"])
+    ScyllaCluster.get_session = lambda: session  # monkey patching to escape need for flask app context
+
+    yield database
+    database.shutdown()
+
+
+@fixture(scope='session')
+def release_manager_service(argus_db) -> ReleaseManagerService:
+    return ReleaseManagerService()
+
+
+@fixture(scope='session')
+def client_service(argus_db):
+    return ClientService()
+
+
+@fixture(scope='session')
+def results_service(argus_db):
+    return ResultsService()
+
+
+def get_fake_test_run(
+        test: ArgusTest,
+        schema_version: str = "1.0.0",
+        job_url: str = "http://example.com",
+        started_by: str = "default_user",
+        commit_id: str = "default_commit_id",
+        sct_config: dict | None = None,
+        origin_url: str | None = None,
+        branch_name: str | None = "main",
+        runner_public_ip: str | None = None,
+        runner_private_ip: str | None = None
+) -> tuple[str, SCTTestRunSubmissionRequest]:
+    run_id = str(uuid.uuid4())
+    return "scylla-cluster-tests", SCTTestRunSubmissionRequest(
+        schema_version=schema_version,
+        run_id=run_id,
+        job_name=test.build_system_id,
+        job_url=job_url,
+        started_by=started_by,
+        commit_id=commit_id,
+        sct_config=sct_config,
+        origin_url=origin_url,
+        branch_name=branch_name,
+        runner_public_ip=runner_public_ip,
+        runner_private_ip=runner_private_ip
+    )
+
+
+@fixture(scope='session')
+def release(release_manager_service) -> ArgusRelease:
+    name = f"best_results_{time.time_ns()}"
+    return release_manager_service.create_release(name, name, False)
+
+
+@fixture(scope='session')
+def group(release_manager_service, release) -> ArgusGroup:
+    name = f"br_group{time.time_ns()}"
+    return release_manager_service.create_group(name, name, build_system_id=release.name, release_id=str(release.id))
+
+
+@fixture
+def fake_test(release_manager_service, group: ArgusGroup, release: ArgusRelease) -> ArgusTest:
+    name = f"test_{time.time_ns()}"
+    return release_manager_service.create_test(name, name, name, name,
+                                               group_id=str(group.id), release_id=str(release.id), plugin_name='scylla-cluster-tests')
---
+++ argus/backend/tests/results_service/test_validation_rules.py
@@ -0,0 +1,203 @@
+import logging
+from dataclasses import asdict, dataclass
+from typing import Any
+from uuid import UUID
+
+from argus.backend.tests.conftest import get_fake_test_run, fake_test
+from argus.client.generic_result import GenericResultTable, ColumnMetadata, ResultType, ValidationRule, Status
+
+LOGGER = logging.getLogger(__name__)
+
+
+class SampleTable(GenericResultTable):
+    class Meta:
+        name = "Test Table Name"
+        description = "Test Table Description"
+        Columns = [ColumnMetadata(name="h_is_better", unit="ms", type=ResultType.FLOAT, higher_is_better=True),
+                   ColumnMetadata(name="l_is_better", unit="ms", type=ResultType.INTEGER, higher_is_better=False),
+                   ColumnMetadata(name="duration col name", unit="s", type=ResultType.DURATION, higher_is_better=False),
+                   ColumnMetadata(name="non tracked col name", unit="", type=ResultType.FLOAT),
+                   ColumnMetadata(name="text col name", unit="", type=ResultType.TEXT),
+                   ]
+        ValidationRules = {"h_is_better": ValidationRule(best_abs=4),
+                           "l_is_better": ValidationRule(best_pct=50),
+                           "duration col name": ValidationRule(fixed_limit=590)
+                           }
+
+
+@dataclass
+class SampleCell:
+    column: str
+    row: str
+    value: Any
+    status: Status = Status.UNSET
+
+
+def results_to_dict(results):
+    return {
+        cell['column']: {
+            cell['row']: {
+                'value': cell['value'] if cell['value'] is not None else cell['value_text'],
+                'status': cell['status']
+            }
+        }
+        for cell in results['cells']
+    }
+
+
+def test_can_track_validation_rules_changes(fake_test, client_service, results_service, release, group):
+    run_type, run = get_fake_test_run(test=fake_test)
+    results = SampleTable()
+    results.sut_timestamp = 123
+    sample_data = [
+        SampleCell(column="h_is_better", row="row", value=10),
+        SampleCell(column="l_is_better", row="row", value=100),
+        SampleCell(column="duration col name", row="row", value=10),
+        SampleCell(column="non tracked col name", row="row", value=10),
+        SampleCell(column="text col name", row="row", value="a value"),
+    ]
+    for cell in sample_data:
+        results.add_result(column=cell.column, row=cell.row, value=cell.value, status=cell.status)
+    client_service.submit_run(run_type, asdict(run))
+    client_service.submit_results(run_type, run.run_id, results.as_dict())
+    run_results = results_service.get_run_results(fake_test.id, UUID(run.run_id))
+    actual_cells = results_to_dict(run_results[0])
+    # all results should be marked as passed
+    for cell in sample_data:
+        if cell.column == "text col name" or cell.column == "non tracked col name":
+            assert actual_cells[cell.column][cell.row]['value'] == cell.value
+            assert actual_cells[cell.column][cell.row]['status'] == "UNSET"
+        else:
+            assert actual_cells[cell.column][cell.row]['value'] == cell.value
+            assert actual_cells[cell.column][cell.row]['status'] == "PASS"
+
+    # test validation rules are applied correctly
+    run_type, run = get_fake_test_run(test=fake_test)
+    results = SampleTable()
+    results.sut_timestamp = 124
+    sample_data = [
+        SampleCell(column="h_is_better", row="row", value=6),
+        SampleCell(column="l_is_better", row="row", value=150),
+        SampleCell(column="duration col name", row="row", value=600),
+        SampleCell(column="non tracked col name", row="row", value=12),
+        SampleCell(column="text col name", row="row", value="a value"),
+    ]
+    for cell in sample_data:
+        results.add_result(column=cell.column, row=cell.row, value=cell.value, status=cell.status)
+    client_service.submit_run(run_type, asdict(run))
+    client_service.submit_results(run_type, run.run_id, results.as_dict())
+    run_results = results_service.get_run_results(fake_test.id, UUID(run.run_id))
+    actual_cells = results_to_dict(run_results[0])
+    for cell in sample_data:
+        if cell.column == "text col name" or cell.column == "non tracked col name":
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "UNSET", f"Expected ERROR for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+        else:
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "ERROR", f"Expected ERROR for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+
+    # new best result appears
+    run_type, run = get_fake_test_run(test=fake_test)
+    results = SampleTable()
+    results.sut_timestamp = 125
+    sample_data = [
+        SampleCell(column="h_is_better", row="row", value=100),
+        SampleCell(column="l_is_better", row="row", value=50),
+        SampleCell(column="duration col name", row="row", value=10),
+        SampleCell(column="non tracked col name", row="row", value=12),
+        SampleCell(column="text col name", row="row", value="a value"),
+    ]
+    for cell in sample_data:
+        results.add_result(column=cell.column, row=cell.row, value=cell.value, status=cell.status)
+    client_service.submit_run(run_type, asdict(run))
+    client_service.submit_results(run_type, run.run_id, results.as_dict())
+    run_results = results_service.get_run_results(fake_test.id, UUID(run.run_id))
+    actual_cells = results_to_dict(run_results[0])
+    for cell in sample_data:
+        if cell.column == "text col name" or cell.column == "non tracked col name":
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "UNSET", f"Expected PASS for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+        else:
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "PASS", f"Expected PASS for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+
+    # validation should be now with new best results
+    run_type, run = get_fake_test_run(test=fake_test)
+    results = SampleTable()
+    results.sut_timestamp = 126
+    sample_data = [
+        SampleCell(column="h_is_better", row="row", value=95),
+        SampleCell(column="l_is_better", row="row", value=75),
+        SampleCell(column="duration col name", row="row", value=590),
+        SampleCell(column="non tracked col name", row="row", value=12),
+        SampleCell(column="text col name", row="row", value="a value"),
+    ]
+    for cell in sample_data:
+        results.add_result(column=cell.column, row=cell.row, value=cell.value, status=cell.status)
+    client_service.submit_run(run_type, asdict(run))
+    client_service.submit_results(run_type, run.run_id, results.as_dict())
+    run_results = results_service.get_run_results(fake_test.id, UUID(run.run_id))
+    actual_cells = results_to_dict(run_results[0])
+    for cell in sample_data:
+        if cell.column == "text col name" or cell.column == "non tracked col name":
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "UNSET", f"Expected ERROR for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+        else:
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "ERROR", f"Expected ERROR for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+
+    # applying new validation rules should be taken into account for next results, new rules are less strict
+    # duration col name rule is removed and 'non tracked col name' has new fixed limit
+    class NewRulesSampleTable(SampleTable):
+        class Meta:
+            name = "Test Table Name"
+            description = "Test Table Description"
+            Columns = [ColumnMetadata(name="h_is_better", unit="ms", type=ResultType.FLOAT, higher_is_better=True),
+                       ColumnMetadata(name="l_is_better", unit="ms", type=ResultType.INTEGER, higher_is_better=False),
+                       ColumnMetadata(name="duration col name", unit="s", type=ResultType.DURATION, higher_is_better=False),
+                       ColumnMetadata(name="non tracked col name", unit="", type=ResultType.FLOAT, higher_is_better=True),
+                       ColumnMetadata(name="text col name", unit="", type=ResultType.TEXT),
+                       ]
+            ValidationRules = {"h_is_better": ValidationRule(best_abs=100),
+                               "l_is_better": ValidationRule(best_pct=90),
+                               "non tracked col name": ValidationRule(fixed_limit=100)  # new rule, removed old one too
+                               }
+    run_type, run = get_fake_test_run(test=fake_test)
+    results = NewRulesSampleTable()
+    results.sut_timestamp = 122
+    sample_data = [
+        SampleCell(column="h_is_better", row="row", value=95),
+        SampleCell(column="l_is_better", row="row", value=75),
+        SampleCell(column="duration col name", row="row", value=691),
+        SampleCell(column="non tracked col name", row="row", value=101),
+        SampleCell(column="text col name", row="row", value="a value"),
+    ]
+    for cell in sample_data:
+        results.add_result(column=cell.column, row=cell.row, value=cell.value, status=cell.status)
+    client_service.submit_run(run_type, asdict(run))
+    client_service.submit_results(run_type, run.run_id, results.as_dict())
+    run_results = results_service.get_run_results(fake_test.id, UUID(run.run_id))
+    actual_cells = results_to_dict(run_results[0])
+    for cell in sample_data:
+        if cell.column == "text col name" :
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "UNSET", f"Expected UNSET for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
+        else:
+            assert actual_cells[cell.column][cell.row][
+                       'value'] == cell.value, f"Expected {cell.value} but got {actual_cells[cell.column][cell.row]['value']}"
+            assert actual_cells[cell.column][cell.row][
+                       'status'] == "PASS", f"Expected PASS for {cell.column} but got {actual_cells[cell.column][cell.row]['status']}"
