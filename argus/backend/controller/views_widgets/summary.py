e7aceb38c8c0153700abd7108b937caf63b76027
Lukasz Sojka <lukasz.sojka@scylladb.com>
2024-11-29 17:44:43 +0100

    feature(summary-widget): summary widget
    View widged showing tests statuses and results per Scylla version.
    Each test can be expanded to see results (tables) with key metrics.
    Initial features:
    1. Results summary by scylla version (date, scm revision)
    2. passed tests are collapsed by default
    3. not run tests marked as warning
    4. hiding tables with all cells green
    ... many more but small ones.
    
    caveats:
    1. works only with scylla-server package
    2. tests filtering based on release (oss vs enterprise) implementation
       is poor as will work only with scylla-master and scylla-enterprise
       argus releases
    3. key metrics are hardcoded, followup issue:
       https://github.com/scylladb/qa-tasks/issues/1821
    
    Did a refactor of ResultsTab - extracted ResultTable to be able to reuse
    it across different parts of Argus (like Summary Widget)
    
    refs: https://github.com/scylladb/argus/issues/486

---
+++ argus/backend/controller/views_widgets/summary.py
@@ -0,0 +1,42 @@
+from uuid import UUID
+
+from flask import Blueprint, request, g
+
+from argus.backend.models.web import ArgusUserView
+from argus.backend.service.results_service import ResultsService
+from argus.backend.service.user import api_login_required
+from argus.backend.util.common import get_payload
+
+bp = Blueprint("summary", __name__, url_prefix="/widgets")
+
+
+@bp.route("/summary/versioned_runs", methods=["GET"])
+@api_login_required
+def get_versioned_runs():
+    view_id = UUID(request.args.get("view_id"))
+    view: ArgusUserView = ArgusUserView.get(id=view_id)
+    service = ResultsService()
+    versioned_runs = service.get_tests_by_version("scylla-server", view.tests)
+    return {
+        "status": "ok",
+        "response": versioned_runs,
+    }
+
+@bp.route("/summary/runs_results", methods=["POST"])
+@api_login_required
+def get_runs_results():
+    versioned_runs = get_payload(request)
+    service = ResultsService()
+    response = {}
+    for test_id, test_methods in versioned_runs.items():
+        response[test_id] = {}
+        for method, run in test_methods.items():
+            response[test_id][method] = {}
+            run_id = run['run_id']
+            response[test_id][method][run_id] = service.get_run_results(UUID(test_id), UUID(run_id), key_metrics=[
+                "P99 read", "P99 write", "duration", "Throughput write", "Throughput read", "allocs_per_op",
+                "cpu_cycles_per_op", "instructions_per_op", "logallocs_per_op"])
+    return {
+        "status": "ok",
+        "response": response,
+    }
