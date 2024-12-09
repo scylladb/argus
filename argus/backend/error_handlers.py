b79062f7bf9b1a5dceccb8f6182c360c9722bb1e
Alexey Kartashov <alexey.kartashov@scylladb.com>
2024-12-09 14:45:34 +0100

    improvement(error_handlers): Add context dump to the exceptions
    This commit adds additional information and trace ids to the exception
    that occur inside the API calls, allowing to collect more information
    about the error, including the request data.
    
    Fixes #492

--- argus/backend/error_handlers.py
+++ argus/backend/error_handlers.py
@@ -1,4 +1,7 @@
+import base64
+from hashlib import sha256
 import logging
+import os
 from traceback import format_exception
 from flask import request
 
@@ -12,14 +15,20 @@ class DataValidationError(APIException):
     pass
 
 def handle_api_exception(exception: Exception):
+    trace_id = base64.encodebytes(sha256(os.urandom(64)).digest()).decode(encoding="utf-8").strip()
     if issubclass(exception.__class__, APIException):
-        LOGGER.info("Endpoint %s responded with error %s: %s", request.endpoint, exception.__class__.__name__, str(exception))
+        LOGGER.info("[TraceId: %s] Endpoint %s responded with error %s: %s", trace_id, request.endpoint, exception.__class__.__name__, str(exception))
+        LOGGER.info("[TraceId: %s] Headers\n%s", trace_id, request.headers)
+        LOGGER.info("[TraceId: %s] Request Data Start\n%s\nRequest Data End", trace_id, request.json if request.is_json else request.get_data(as_text=True))
     else:
-        LOGGER.error("Exception in %s\n%s", request.endpoint, "".join(format_exception(exception)))
+        LOGGER.error("[TraceId: %s] Exception in %s\n%s", trace_id, request.endpoint, "".join(format_exception(exception)))
+        LOGGER.error("[TraceId: %s] Headers\n%s", trace_id, request.headers)
+        LOGGER.error("[TraceId: %s] Request Data Start\n%s\nRequest Data End", trace_id, request.json if request.is_json else request.get_data(as_text=True))
 
     return {
         "status": "error",
         "response": {
+            "trace_id": trace_id,
             "exception": exception.__class__.__name__,
             "arguments": exception.args
         }
