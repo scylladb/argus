0dfe96b58ad7ba2e559519a007c3b49245d3eb70
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-02-27 12:44:05 +0100

    fix(frontend/argus.css): Force lightmode for markdown comments
    This temporary fix (until #531 is done) will force markdown to be always
    rendered in light mode, preventing issues when users have their browser
    theme set to Dark.
    
    Fixes #598

--- frontend/argus.js
+++ frontend/argus.js
@@ -1,5 +1,5 @@
 import {} from "bootstrap";
-import "github-markdown-css";
+import "github-markdown-css/github-markdown-light.css";
 import "./argus.css";
 
 export const applicationCurrentUser = gArgusCurrentUser ?? {};
