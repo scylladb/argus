b61790d7ca652ab7fcc0ef1093a440483f4b4a93
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-03-17 13:40:45 +0100

    improvement(WorkArea): Add Test Execute button
    This commits adds a new button to the test runs within argus, allowing
    user to either start a build from scratch or rebuild a latest run
    without having to open a run to do so. The from scratch implementation
    will try to retrieve previous build parameters (In case previous build
    wasn't submitted to argus) and if that's not possible, will offer user
    to start a build without parameters to be able to retrieve the build on
    subsequent call.
    
    Fixes #542

---
+++ frontend/TestRun/Jenkins/BuildConfirmationDialog.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    /**
+     * @type {Object} args
+     */
+    export let args;
+    const dispatch = createEventDispatcher();
+</script>
+
+<div>
+    This test contains no runs and no runs were found inside Jenkins. A build will be started without parameters to later retrieve them. Do you wish to continue?
+    <div class="p-2">
+        <button class="btn btn-primary me-1" on:click={() => dispatch("exit", { confirm: true })}>OK</button>
+        <button class="btn btn-secondary" on:click={() => dispatch("exit", { confirm: false })}>Cancel</button>
+    </div>
+</div>
