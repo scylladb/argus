e635b061dbeb27a25af30035c5351df918c366ff
Israel Fruchter <fruch@scylladb.com>
2025-03-20 12:05:49 +0200

    Add option to pass on extra headers into Argus clients
    for purpose of identificaton of the code part sending out results
    into argus, we should be able to configure as needed the headers
    the client are sending out.
    
    this change introducs extra argument to clients, and to thier CLIs

--- argus/client/generic/cli.py
+++ argus/client/generic/cli.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 from pathlib import Path
 import click
 import logging
@@ -7,6 +8,15 @@ from argus.client.base import TestStatus
 from argus.client.generic.client import ArgusGenericClient
 
 LOGGER = logging.getLogger(__name__)
+
+def validate_extra_headers(ctx, param, value):
+    if isinstance(value, dict):
+        return value
+
+    try:
+        return json.loads(value)
+    except JSONDecodeError as ex:
+        raise click.BadParameter(f"--extra-headers should be in json format:\n\n{value}\n\n{ex}")
 
 
 @click.group
@@ -22,9 +32,10 @@ def cli():
 @click.option("--build-url", required=True, help="Job URL in the build system")
 @click.option("--started-by", required=True, help="Username of the user who started the job")
 @click.option("--scylla-version", required=False, default=None, help="Version of Scylla used for this job")
-def submit_run(api_key: str, base_url: str, id: str, build_id: str, build_url: str, started_by: str, scylla_version: str = None):
+@click.option("--extra-headers", default={}, type=click.UNPROCESSED, callback=validate_extra_headers, help="extra headers to pass to argus, should be in json format")
+def submit_run(api_key: str, base_url: str, id: str, build_id: str, build_url: str, started_by: str, scylla_version: str = None, extra_headers: dict):
     LOGGER.info("Submitting %s (%s) to Argus...", build_id, id)
-    client = ArgusGenericClient(auth_token=api_key, base_url=base_url)
+    client = ArgusGenericClient(auth_token=api_key, base_url=base_url, extra_headers=extra_headers)
     client.submit_generic_run(build_id=build_id, run_id=id, started_by=started_by, build_url=build_url, scylla_version=scylla_version)
     LOGGER.info("Done.")
 
@@ -35,8 +46,8 @@ def submit_run(api_key: str, base_url: str, id: str, build_id: str, build_url: s
 @click.option("--id", required=True, help="UUID (v4 or v1) unique to the job")
 @click.option("--status", required=True, help="Resulting job status")
 @click.option("--scylla-version", required=False, default=None, help="Version of Scylla used for this job")
-def finish_run(api_key: str, base_url: str, id: str, status: str, scylla_version: str = None):
-    client = ArgusGenericClient(auth_token=api_key, base_url=base_url)
+def finish_run(api_key: str, base_url: str, id: str, status: str, scylla_version: str = None, extra_headers: dict):
+    client = ArgusGenericClient(auth_token=api_key, base_url=base_url, extra_headers=extra_headers)
     status = TestStatus(status)
     client.finalize_generic_run(run_id=id, status=status, scylla_version=scylla_version)
 
@@ -48,8 +59,8 @@ def finish_run(api_key: str, base_url: str, id: str, status: str, scylla_version
 @click.option("--plan-id", help="Specific plan id for filtering", default=None, required=False)
 @click.option("--release", help="Release name to filter plans by", default=None, required=False)
 @click.option("--job-info-file", required=True, help="JSON file with trigger information (see detailed docs)")
-def trigger_jobs(api_key: str, base_url: str, job_info_file: str, version: str, plan_id: str, release: str):
-    client = ArgusGenericClient(auth_token=api_key, base_url=base_url)
+def trigger_jobs(api_key: str, base_url: str, job_info_file: str, version: str, plan_id: str, release: str, extra_headers: dict):
+    client = ArgusGenericClient(auth_token=api_key, base_url=base_url, extra_headers=extra_headers)
     path = Path(job_info_file)
     if not path.exists():
         LOGGER.error("File not found: %s", job_info_file)
