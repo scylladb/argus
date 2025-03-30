ebaa6ea95b3717c55af9164fc0eb3920c9972fc3
Lukasz Sojka <lukasz.sojka@scylladb.com>
2025-03-30 13:15:53 +0200

    feature(nemesis_widget): allow drill down to test runs
    When viewing nemesis stats, user might want to drill down to failures to
    see more details.
    
    Added possibility to click on nemesis bar (on the graph) to show tabular
    data about failed/passed runs - similar like it was in ES.
    User may go to specific test run or just view stack traces of nemesis
    failures.
    
    closes: https://github.com/scylladb/argus/issues/626

--- argus/backend/service/views_widgets/nemesis_stats.py
+++ argus/backend/service/views_widgets/nemesis_stats.py
@@ -1,34 +1,34 @@
-from dataclasses import dataclass
 from uuid import UUID
 
 
 from argus.backend.db import ScyllaCluster
 from argus.backend.plugins.sct.testrun import SCTTestRun
 
-@dataclass
-class NemesisStats:
-    version: str
-    name: str
-    duration: int
-    status: str
-
 
 class NemesisStatsService:
-
     def __init__(self) -> None:
         self.cluster = ScyllaCluster.get()
 
     def get_nemesis_data(self, test_id: UUID):
         rows = SCTTestRun.filter(test_id=test_id).only(["id", "nemesis_data", "investigation_status", "packages"]).all()
         nemesis_data = []
-        for test in [row for row in rows if row["investigation_status"].lower() != "ignored"]:
+        for run in [row for row in rows if row["investigation_status"].lower() != "ignored"]:
             try:
-                version = [package.version for package in test["packages"] if package.name == "scylla-server"][0]
+                version = [package.version for package in run["packages"] if package.name == "scylla-server"][0]
             except (IndexError, TypeError):
                 continue
-            if not test["nemesis_data"]:
+            if not run["nemesis_data"]:
                 continue
-            for nemesis in [nemesis for nemesis in test["nemesis_data"]  if nemesis.status in ("succeeded", "failed")]:
-                nemesis_data.append({"version": version, "name": nemesis.name.split("disrupt_")[-1], "duration": nemesis.end_time - nemesis.start_time,
-                                    "status": nemesis.status})
+            for nemesis in [nemesis for nemesis in run["nemesis_data"] if nemesis.status in ("succeeded", "failed")]:
+                nemesis_data.append(
+                    {
+                        "version": version,
+                        "name": nemesis.name.split("disrupt_")[-1],
+                        "start_time": nemesis.start_time,
+                        "duration": nemesis.end_time - nemesis.start_time,
+                        "status": nemesis.status,
+                        "run_id": run["id"],
+                        "stack_trace": nemesis.stack_trace,
+                    }
+                )
         return nemesis_data
