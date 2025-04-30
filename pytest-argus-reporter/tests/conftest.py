bd7ae3cc4e8a6d96712b5a0e935db1bc8d8e3d2e
Israel Fruchter <fruch@scylladb.com>
2025-04-30 15:56:16 +0300

    Intial version of pytest-argus-reporter
    this is a dumb down version of pytest-elk-reporter
    which depends on `argus-alm`
    
    * unit tests are working (except one)
    * all the docs were updated to point to Argus and not elastic
    
    actul API calls to Argus are yet missing

---
+++ pytest-argus-reporter/tests/conftest.py
@@ -0,0 +1,50 @@
+import re
+
+import pytest
+import requests_mock as rm_module
+
+
+pytest_plugins = "pytester"  # pylint: disable=invalid-name
+
+
+@pytest.fixture(scope="function")  # executed on every test
+def requests_mock():
+    """Mock out the requests component of your code with defined responses.
+    Mocks out any requests made through the python requests library with useful
+    responses for unit testing. See:
+    https://requests-mock.readthedocs.io/en/latest/
+    """
+    kwargs = {"real_http": False}
+
+    with rm_module.Mocker(**kwargs) as mock:
+        yield mock
+
+
+@pytest.fixture(scope="function", autouse=True)
+def configure_es_mock(requests_mock):  # pylint: disable=redefined-outer-name
+    matcher = re.compile(r"http://argus.scylladb.com:443/api/v1/clientreport_test.*")
+
+    requests_mock.post(
+        matcher,
+        status_code=201
+    )
+    #    response_list=[
+    #        dict(
+    #            text='{"aggregations": {"percentiles_duration": {"values": {"95.0": 60.0}}}}',
+    #            status_code=201,
+    #        ),
+    #        dict(
+    #            text='{"aggregations": {"percentiles_duration": {"values": {"95.0": 60.0}}}}',
+    #            status_code=201,
+    #        ),
+    #        dict(
+    #            text='{"aggregations": {"percentiles_duration": {"values": {"95.0": 120.0}}}}',
+    #            status_code=201,
+    #        ),
+    #        dict(
+    #            text='{"aggregations": {"percentiles_duration": {"values": {"95.0": null}}}}',
+    #            status_code=201,
+    #        ),
+    #        dict(text="should error !!!", status_code=500),
+    #    ],
+    #)
