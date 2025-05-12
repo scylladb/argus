ff04cd61dd2c10e40696771ece9636207e2d0922
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-05-12 13:25:27 +0200

    improvement(release_planner): Allow user to attach an existing view
    This commit adds additional logic to support attaching an existing view
    to a newly created (or one being edited) plan. Attached view will
    preserve its name and description, as well as widgets, but its selection
    will be replaced by plan's selection. Additionally, when deleting a
    plan, a new checkbox has been added to prevent attached view from being
    deleted (in this case it just detaches and becomes a free-standing
    view). To handle corner cases, Admin users can now also edit the plan_id
    field of views inside view manager, in case manual intervention is
    required.
    
    For views, it is assumed that they only linked to 1 plan and cannot be
    relinked from a plan without being detached first. The detachment can be
    done in two ways:
     * Create a new view and attach it to a plan, thereby detaching the
       previous view
     * Remove view from selection inside Plan Editor, in which case a new
       view (automatic) will be created for the plan
    
    Fixes #610

--- argus/backend/controller/planner_api.py
+++ argus/backend/controller/planner_api.py
@@ -145,7 +145,8 @@ def copy_plan():
 @bp.route("/plan/<string:plan_id>/delete", methods=["DELETE"])
 @api_login_required
 def delete_plan(plan_id: str):
-    result = PlanningService().delete_plan(plan_id)
+    delete_view = bool(int(request.args.get("deleteView", "0")))
+    result = PlanningService().delete_plan(plan_id, delete_view=delete_view)
 
     return {
         "status": "ok",
