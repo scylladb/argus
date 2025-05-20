bafcbadb40d6ebcb0d46157a2febb91f277ad9a0
Dusan Malusev <dusan@dusanmalusev.dev>
2025-05-20 11:50:18 +0200

    feature(pytest): initial implementation for pytest part
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

---
+++ frontend/TestRun/Pytest/PytestTab.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { faEye } from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
+
+    export let testRun;
+</script>
+
+<button
+    class="nav-link"
+    id="nav-gemini-tab-{testRun.id}"
+    data-bs-toggle="tab"
+    data-bs-target="#nav-gemini-{testRun.id}"
+    type="button"
+    role="tab"
+    ><Fa icon={faEye}/> PyTest </button
+>
