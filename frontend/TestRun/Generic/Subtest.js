cedfde35164e3018da5c08190cdebb1934891bd6
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-07-01 14:11:03 +0200

    feature(Generic/Subtest): Add Dtest tab

--- frontend/TestRun/Generic/Subtest.js
+++ frontend/TestRun/Generic/Subtest.js
@@ -1,13 +1,17 @@
 import { PytestTab, PytestRun } from "../Pytest";
+import DtestTab from "../Pytest/DtestTab.svelte";
 
 export const Cases = {
     PYTEST: "pytest",
+    DTEST: "dtest",
 };
 
 export const Tabs = {
     [Cases.PYTEST]: PytestTab,
+    [Cases.DTEST]: DtestTab,
 };
 
 export const TabBody = {
     [Cases.PYTEST]: PytestRun,
+    [Cases.DTEST]: PytestRun,
 };
