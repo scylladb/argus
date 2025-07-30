4849fab3403aaed1bb166465ff9a7bf2bac8a482
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-07-30 20:09:02 +0200

    fix(argus_service): Hide disabled jobs when requesting planned jobs

--- argus/backend/service/argus_service.py
+++ argus/backend/service/argus_service.py
@@ -695,14 +695,12 @@ class ArgusService:
 
         last_runs: dict[UUID, Model] = {}
         for test in resolved:
-            if not test.enabled:
-                continue
             try:
                 last_runs[test.id] = AVAILABLE_PLUGINS[test.plugin_name].model.filter(build_id=test.build_system_id).limit(1).get()
             except PluginModelBase.DoesNotExist:
                 last_runs[test.id] = None
 
-        return [{**dict(test.items()), "last_run": last_runs.get(test.id) } for test in resolved]
+        return [{**dict(test.items()), "last_run": last_runs.get(test.id) } for test in resolved if test.enabled]
 
     def get_schedules_for_user(self, user: User) -> list[dict]:
         all_assigned_schedules = ArgusScheduleAssignee.filter(assignee=user.id).all()
