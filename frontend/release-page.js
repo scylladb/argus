89533da54392c98c1e006314132a8578bfddf079
Alexey Kartashov <alexey.kartashov@scylladb.com>
2025-08-04 12:10:40 +0200

    fix(release-page.js): Disable shorthand stats
    This commit disables stat bars found on the "Release" overview page, as
    they can be quite costly to run for not much benefit

--- frontend/release-page.js
+++ frontend/release-page.js
@@ -4,12 +4,13 @@ import StatsFetcher from "./Stats/StatsFetcher.svelte";
 const registeredApps = [];
 const releaseElements = document.querySelectorAll("div.release-card");
 
-releaseElements.forEach(el => {
-    let app = new StatsFetcher({
-        target: el.querySelector("div.release-stats"),
-        props: {
-            releaseName: el.dataset.argusReleaseName
-        }
-    });
-    registeredApps.push(app);
-});
+// disabled due to performance overhead
+// releaseElements.forEach(el => {
+//     let app = new StatsFetcher({
+//         target: el.querySelector("div.release-stats"),
+//         props: {
+//             releaseName: el.dataset.argusReleaseName
+//         }
+//     });
+//     registeredApps.push(app);
+// });
