4094009f62ca82a2050f820e308df042e700f6d7
Alexey Kartashov <alexey.kartashov@scylladb.com>
2026-02-23 16:43:54 +0100

    improvement(SctSetup.svelte): Collapse and max-height for stress cmds
    This commit changes the frame used to display stress commands. It is now
    limited to 768px, same as configs and collapsed by default. A new button
    was added to expand the window.

--- frontend/TestRun/SCT/SctSetup.svelte
+++ frontend/TestRun/SCT/SctSetup.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Fa from "svelte-fa";
     import type { SCTTestRun, StressCommand } from "../TestRun.svelte";
-    import { faCopy } from "@fortawesome/free-solid-svg-icons";
+    import { faAngleDown, faAngleUp, faCopy } from "@fortawesome/free-solid-svg-icons";
     import { timestampToISODate } from "../../Common/DateUtils";
     import { sendMessage } from "../../Stores/AlertStore";
     import { onMount } from "svelte";
@@ -12,6 +12,7 @@
 
     let stressCommands: StressCommand[] = $state([]);
     let init = $state(false);
+    let collapsed = $state(true);
 
     const fetchStressCommands = async function () {
         try {
@@ -41,49 +42,52 @@
 
 <div class="p-2">
     <div class="mb-2">
-        <h4>Stress Commands</h4>
+        <h4><button class="btn btn-sm btn-secondary d-inline-block me-2" onclick={() => (collapsed = !collapsed)}><Fa icon={collapsed ? faAngleUp : faAngleDown}/></button>Stress Commands
+        </h4>
         <p class="text-muted" style="font-size: 0.8rem">
             Stress commands executed during the test run.
         </p>
         {#if stressCommands.length > 0 && init}
-            <table class="table table-striped table-responsive table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Command</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each stressCommands as cmd, idx}
+            <div class:d-none={collapsed} class="rounded bg-light-two p-1" style="max-height: 768px; overflow-y: scroll;">
+                <table class="table table-striped table-responsive table-hover">
+                    <thead>
                         <tr>
-                            <td class="fw-bold">{idx+1}</td>
-                            <td>
-                                <div class="input-group">
-                                    <textarea class="form-control" style="resize: none" rows=4 disabled>{cmd.cmd}</textarea>
-                                    <button class="d-none btn btn-success btn-sm" onclick={() => navigator.clipboard.writeText(cmd.cmd)}><Fa icon={faCopy}/></button>
-                                </div>
-                                <div class="d-flex">
-                                    {#if cmd.log_name}
-                                        <div>
-                                            Log File: {cmd.log_name}
-                                        </div>
-                                    {/if}
-                                    {#if cmd.loader_name}
-                                        <div class="ms-auto">
-                                            Node: {cmd.loader_name}
-                                        </div>
-                                    {/if}
-                                </div>
-                                {#if cmd.ts}
-                                    <div class="text-muted" style="font-size: 0.75rem">
-                                        Executed at {timestampToISODate(cmd.ts)}
-                                    </div>
-                                {/if}
-                            </td>
+                            <th>#</th>
+                            <th>Command</th>
                         </tr>
-                    {/each}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {#each stressCommands as cmd, idx}
+                            <tr>
+                                <td class="fw-bold">{idx+1}</td>
+                                <td>
+                                    <div class="input-group">
+                                        <textarea class="form-control" style="resize: none" rows=4 disabled>{cmd.cmd}</textarea>
+                                        <button class="d-none btn btn-success btn-sm" onclick={() => navigator.clipboard.writeText(cmd.cmd)}><Fa icon={faCopy}/></button>
+                                    </div>
+                                    <div class="d-flex">
+                                        {#if cmd.log_name}
+                                            <div>
+                                                Log File: {cmd.log_name}
+                                            </div>
+                                        {/if}
+                                        {#if cmd.loader_name}
+                                            <div class="ms-auto">
+                                                Node: {cmd.loader_name}
+                                            </div>
+                                        {/if}
+                                    </div>
+                                    {#if cmd.ts}
+                                        <div class="text-muted" style="font-size: 0.75rem">
+                                            Executed at {timestampToISODate(cmd.ts)}
+                                        </div>
+                                    {/if}
+                                </td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
         {:else if stressCommands.length == 0 && !init}
             <div class="text-center text-muted p-4">
                 <span class="spinner-border spinner-border-sm"></span> Fetching stress commands...
