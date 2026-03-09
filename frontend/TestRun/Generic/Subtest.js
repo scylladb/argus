36813e833138df560714a35d225bd6543c7deb02
Alexey Kartashov <alexey.kartashov@scylladb.com>
2026-03-09 15:15:01 +0100

    fix(Subtest.js): Add testpy Tab

--- frontend/TestRun/Generic/Subtest.js
+++ frontend/TestRun/Generic/Subtest.js
@@ -1,5 +1,6 @@
 import { PytestTab, PytestRun } from "../Pytest";
 import DtestTab from "../Pytest/DtestTab.svelte";
+import TestPyTab from "../Pytest/TestPyTab.svelte";
 
 export const Cases = {
     PYTEST: "pytest",
@@ -10,7 +11,7 @@ export const Cases = {
 export const Tabs = {
     [Cases.PYTEST]: PytestTab,
     [Cases.DTEST]: DtestTab,
-    [Cases.TESTPY]: PytestTab,
+    [Cases.TESTPY]: TestPyTab,
 };
 
 export const TabBody = {
---
+++ frontend/TestRun/Pytest/TestPyTab.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { faEye } from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
+    import {createEventDispatcher} from "svelte";
+    import {Cases} from "../Generic/Subtest";
+
+    let { testRun } = $props();
+
+    const dispatcher = createEventDispatcher();
+
+</script>
+
+<button
+    class="nav-link"
+    id="nav-gemini-tab-{testRun}"
+    data-bs-toggle="tab"
+    data-bs-target="#nav-gemini-{testRun}"
+    type="button"
+    role="tab"
+    onclick={() => dispatcher("tab-switched", {tab: Cases.TESTPY})}
+    ><Fa icon={faEye}/> test.py </button
+>
