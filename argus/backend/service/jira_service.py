3e82e5943a77d4dfcf45894e69c0fe95b78318f6
Alexey Kartashov <alexey.kartashov@scylladb.com>
2026-08-04 14:50:00 +0200

    refactor(backend): introduce coodie ORM and port RuntimeStore
    First step of the cqlengine -> coodie migration. Registers a coodie
    driver in ScyllaCluster (wrapping cqlengine's open session to avoid
    replaying UDT registrations against a not-yet-synced schema) and ports
    RuntimeStore to a coodie Document as the porting template: schema-
    compatible field markers, DocumentNotFound at call sites, and a new
    USED_COODIE_MODELS registry synced from sync_core_tables.

--- argus/backend/service/jira_service.py
+++ argus/backend/service/jira_service.py
@@ -11,6 +11,8 @@ from flask import current_app, g
 from jira import JIRA
 
 from argus.backend.models.jira import JiraIssue
+from coodie.exceptions import DocumentNotFound
+
 from argus.backend.models.runtime_store import RuntimeStore
 from argus.backend.models.web import ArgusEventTypes, ArgusTest, ArgusUserView, invalidate_release_snapshots
 from argus.backend.models.github_issue import IssueLink, IssueLabel
@@ -47,9 +49,8 @@ class JiraService:
     def refresh_stale_issues(self):
         try:
             last_ran = RuntimeStore.get(key=self.LAST_RAN_KEY)
-        except RuntimeStore.DoesNotExist:
-            last_ran = RuntimeStore()
-            last_ran.key = self.LAST_RAN_KEY
+        except DocumentNotFound:
+            last_ran = RuntimeStore(key=self.LAST_RAN_KEY)
             last_ran.value = datetime(year=2025, month=1, day=1, hour=0, minute=0, tzinfo=UTC)
             last_ran.save()
 
