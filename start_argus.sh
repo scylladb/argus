09bce0d8657e912ccf0390adc4ce9d1af047a7cb
Alexey Kartashov <alexey.kartashov@scylladb.com>
2026-09-14 14:54:09 +0200

    fix(startup): Set metrics mode on startup, add default value
    This fixes an issue where previously the PROMETHEUS_MULTIPROC_DIR would
    be set by a metrics module based on environment, now it is set by
    default as gunicorn uses same server for both dev and prod.
    
    Also, updated example NGINX configuration.
    
    Fixes ARGUS-229

--- start_argus.sh
+++ start_argus.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PATH="$HOME/.local/bin:$PATH"
+export PROMETHEUS_MULTIPROC_DIR="/tmp/promdb-argus-metrics"
 
-if [[ ! -d "/tmp/promdb-argus-metrics" ]]; then
-    mkdir /tmp/promdb-argus-metrics
+if [[ ! -d "${PROMETHEUS_MULTIPROC_DIR}" ]]; then
+    mkdir "${PROMETHEUS_MULTIPROC_DIR}"
 fi
 
 exec uv run gunicorn -c gunicorn.conf.py 'argus_backend:create_app()'
